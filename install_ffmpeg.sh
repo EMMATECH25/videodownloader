7878f4e047e4fad46b704be56968e5698172872d
unknown <nwogeemmanuel917@gmail.com>
2025-02-17 21:13:22 +0100

    Fix install script for yt-dlp and ffmpeg

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 # Set up local bin directory
-mkdir -p ./bin
-export PATH="./bin:$PATH"
+INSTALL_DIR="/opt/render/project/src/bin"
+mkdir -p "$INSTALL_DIR"
 
-# Download yt-dlp (save inside project folder)
+# Set permissions
+chmod 755 "$INSTALL_DIR"
+
+# Download yt-dlp (save inside bin)
 echo "Downloading yt-dlp..."
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./bin/yt-dlp
-chmod +x ./bin/yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o "$INSTALL_DIR/yt-dlp"
+chmod +x "$INSTALL_DIR/yt-dlp"
 
-# Download ffmpeg (save inside project folder)
+# Download ffmpeg (save inside bin)
 echo "Downloading FFmpeg..."
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 tar -xJf ffmpeg.tar.xz
-mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
-mv ffmpeg-*-static/ffprobe ./bin/ffprobe
-chmod +x ./bin/ffmpeg ./bin/ffprobe
+mv ffmpeg-*-static/ffmpeg "$INSTALL_DIR/ffmpeg"
+mv ffmpeg-*-static/ffprobe "$INSTALL_DIR/ffprobe"
+chmod +x "$INSTALL_DIR/ffmpeg" "$INSTALL_DIR/ffprobe"
 
 # Cleanup
 rm -rf ffmpeg-*-static ffmpeg.tar.xz
 
+# Confirm installation
 echo "Installation complete. yt-dlp and ffmpeg are ready to use."
+ls -l "$INSTALL_DIR"
